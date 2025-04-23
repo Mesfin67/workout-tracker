571d4e3ae35aaee4c40d9aa497b1bf8893a3888e
Mesfin67 <mesfinbebulo@gmail.com>
2025-04-23 18:26:26 +0300

    Update exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -6,19 +6,19 @@ const exercises = [
     {
       id: '1',
       name: 'Push Up',
-      imageUrl: 'https://learn.athleanx.com/wp-content/uploads/2021/12/AX_PERFECT-PUSH-UP-830x467.jpg',
+      imageUrl: './images/pushup.jpg',
       description: 'A basic push-up to build upper body strength.'
     },
     {
       id: '2',
       name: 'Squat',
-      imageUrl: 'https://www.healthline.com/hlcmsresource/images/AN_images/squat-steps-1296x728-squat-steps.jpg',
+      imageUrl: './images/squat.jpg',
       description: 'A squat exercise to strengthen your legs and glutes.'
     },
     {
       id: '3',
       name: 'Plank',
-      imageUrl: 'https://static01.nyt.com/images/2023/02/03/multimedia/WELL-CORE-WORKOUTS0-pqlt/WELL-CORE-WORKOUTS0-pqlt-articleLarge.jpg',
+      imageUrl: './images/plank.jpg',
       description: 'A core stability exercise that develops endurance in your abs and lower back.'
     }
 ];
